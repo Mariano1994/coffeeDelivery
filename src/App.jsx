2ea39fbf9cb50bd0d0decf63f6ce54a0d871efa2
Mariano Capiliku <marianocapiliku@gmail.com>
2024-05-09 20:02:53 +0100

    extends tailwind theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <>
-      <div>
+      <div className=" font-['baloo_2']">
         <h1>Coffee Delivery</h1>
       </div>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "yellow-dark": "#c47f17",
+        yellow: "#DBAC2C",
+        "yellow-light": "#f1e9c9",
+        "purple-dark": "#4b2995",
+        purple: "#8047f8",
+        "purple-light": "#ebe5f9",
+        "base-title": "#272221",
+        "base-subtitle": "#403937",
+        "base-text": "#574f4d",
+        "base-label": "#8d8686",
+        background: "#fafafa",
+      },
+      fontFamily: {
+        headingFont: "Baloo+2",
+        textFont: "roboto",
+      },
+    },
   },
   plugins: [],
 };
