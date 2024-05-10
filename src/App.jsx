fe39c79ae3bf7859d7c7bde46e525cf125fe324d
Mariano Capiliku <marianocapiliku@gmail.com>
2024-05-10 13:56:25 +0100

    crate home and cart pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <div className=" bg-background h-screen w-screen">
         <Header />
-        <HeroSection />
+        <Home />
       </div>
     </>
   );
